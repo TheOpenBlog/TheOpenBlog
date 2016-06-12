2c4200a41f49421dede6cd467e63a929dafb2d28
PSNAppz <psnappz@gmail.com>
2016-06-12 20:24:37 +0530

    Final Fix
    Test case

--- src/test/indexTest.php
+++ src/test/indexTest.php
@@ -1,12 +1,29 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'].'src/main/index.php');
+$path =__DIR__.'/../main/index.php';
+include_once($path);
 #Testing class for index.php
 class myClassTest extends PHPUnit_Framework_TestCase{
-    public function testYolo(){
-        $args =2;
-        $expectedResult = "Bolo";
-        $result = myfun($args);
-        $this->assertEquals($expectedResult, $result);
+    /**
+    * @param integer $var1 Value
+    * @param integer $var2 What we expect our result to be
+    * @dataProvider yoloMagic The Magic Data Provider
+    */
+    public function testYolo($var1,$var2){
+        $a=new myClass();
+        $args =$var1;
+        $expectedResult =$var2;
+        $result = $a->myfun($args);
+        $this->assertEquals($expectedResult,$result);
     }
+
+    public function yoloMagic()
+{
+    return array(
+        array('1', 'Bolo'),
+        array('2', 'Bolo'),
+        array('3', 'Bolo'),
+        array('4', 'Yolo')
+    );
+}
 }
  ?>
